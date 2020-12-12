f1601ef80de0fdba079598e941a1589343e9ece6
pkotzbach <pawelkotzbach@gmail.com>
2020-12-12 20:53:19 +0100

    Added /nick command and fixed bug

--- CommandCentre.cpp
+++ CommandCentre.cpp
@@ -6,13 +6,17 @@ void CommandCentre::receiveCommand(QStringList& commandList, QString& command)
         main_widget->getChatBox()->clear();
     }
     else if (commandList.at(0).compare(QString("connect")) == 0 || commandList.at(0).compare(QString("c")) == 0){
-        connection_manager->connectToHost(commandList.at(1), options->getPort());
+        if (commandList.size() > 1) connection_manager->connectToHost(commandList.at(1), options->getPort());
     }
     else if (commandList.at(0).compare(QString("disconnect")) == 0){
         connection_manager->disconnectFromHost();
     }
     else if (commandList.at(0).compare(QString("join")) == 0){
-        options->channel = commandList.at(1);
+        if (commandList.size() > 1) options->channel = commandList.at(1);
+        connection_manager->writeToSocket(command);
+    }
+    else if (commandList.at(0).compare(QString("nick")) == 0){
+        if (commandList.size() > 1) options->nick = commandList.at(1).toStdString();
         connection_manager->writeToSocket(command);
     }
     else connection_manager->writeToSocket(command);
